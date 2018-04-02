318496f6a5db7c9756494c9bcb60e01094138f83
David Sawatske <sawatskeda10@gmail.com>
2018-04-02 09:28:53 -0500

    Add name attr to registration with first and last virtual attrs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+
+  def first_name
+   self.name.split.first
+  end
+
+  def last_name
+   self.name.split.last
+  end
 end
