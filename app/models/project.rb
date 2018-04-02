4d11fa6dc234b51c0872e4f55836b55c3e63be60
David Sawatske <sawatskeda10@gmail.com>
2018-04-02 10:04:28 -0500

    Add Active Record Validations and image default to Project models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+  validates :title, :description, :image, presence: true
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.image ||= "http://placehold.it/600x400"
+  end
 end
