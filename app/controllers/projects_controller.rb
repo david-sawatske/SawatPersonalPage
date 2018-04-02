809c7f155274680d7abee4d7f9a67cefb20c31d7
David Sawatske <sawatskeda10@gmail.com>
2018-04-02 10:04:48 -0500

    Add #create and #new actions to ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,21 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @projects = Project.new(project_params)
+
+    if @projects.save
+      redirect_to projects_path, notice: 'Project created'
+    else
+      flash.now[:errors] = @projects.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title, :description, :image)
+    end
 end
