6f2d05e3d5daf552eec224bf4cd79fcae821f6d4
David Sawatske <sawatskeda10@gmail.com>
2018-04-02 10:18:24 -0500

    Add remaining Project actions setting a set_filter before_action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,10 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @projects = Project.all
+  end
+
   def new
     @project = Project.new
   end
@@ -14,7 +20,32 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path, notice: 'Project was successfully updated'
+    else
+      flash.now[:errors] = @project.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @project.destroy
+
+    redirect_to projects_url, notice: 'Project removed'
+  end
+
   private
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
     def project_params
       params.require(:project).permit(:title, :description, :image)
     end
