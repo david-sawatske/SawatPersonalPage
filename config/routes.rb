98e0dac0caeded86b93185c0297fd62488a0ccea
David Sawatske <sawatskeda10@gmail.com>
2018-04-28 21:59:09 -0500

    Create Blogs Controller with #index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
+  resources :blogs
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout',
                                            sign_up: 'register' }
   root 'pages#home'
