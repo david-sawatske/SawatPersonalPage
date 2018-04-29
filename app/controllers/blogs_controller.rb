14087b7571cf17a9234a5070bee3397bf7c9aae0
David Sawatske <sawatskeda10@gmail.com>
2018-04-28 22:17:25 -0500

    Build ability to create new Blogs from UI

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,24 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+
+    if @blog.save
+      redirect_to @blog, notice: 'Blog was successfully created.'
+    else
+      flash.now[:errors] = @blog.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :body, :project_id)
+    end
 end
