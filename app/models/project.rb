11f74fc965dafe7d0fe81e76407c1ccef0f06f97
David Sawatske <sawatskeda10@gmail.com>
2018-05-06 19:19:04 -0500

    Add technologies as nested attr to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                allow_destroy: true,
+                                reject_if: lambda { |tech| tech['name'].blank? }
   has_many :blogs
   validates :title, :description, presence: true
 
