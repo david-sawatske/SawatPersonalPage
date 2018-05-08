6741fae8b55d80d05deda597b5612da20ed94736
David Sawatske <sawatskeda10@gmail.com>
2018-05-07 19:35:11 -0500

    Add backend persistence for Project.position attribute via drag and drop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  resources :projects
   resources :blogs
 
   devise_for :users, path: '', path_names: { sign_in: 'login',
                                              sign_out: 'logout',
                                              sign_up: 'register' }
+
+  resources :projects, except: [:show] do
+    put :sort, on: :collection
+  end
+
   root 'pages#home'
 end
